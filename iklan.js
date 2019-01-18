ca8ecccfcb4fc8fa91e3c5af8ba51c35a210cc79
tasikisme <btwanyway@yahoo.com>
2019-01-18 22:33:27 +0700

    Add files via upload

---
+++ iklan.js
@@ -0,0 +1,28 @@
+//<![CDATA[
+
+var img_width = "250";
+var img_height = "250";
+
+
+var ad=new Array()
+ad[0]='http://4.bp.blogspot.com/-zD__p6X-WUE/U6pXW7QcctI/AAAAAAAADQw/kz3y0_qy7QQ/s1600/autotexttebaru_250x250.png';
+ad[1]='http://2.bp.blogspot.com/-BuGR6b7KRY0/VWpktb05sTI/AAAAAAAAFkk/JZpwLYuo9lU/s1600/buatdpbbmkeren.gif';
+ad[2]='https://2.bp.blogspot.com/-qr1N2O8YeDQ/WfsAUL4krhI/AAAAAAAALVE/s3QjGeVqDG4gk8xeARUXPiwKKA1BUwRQQCLcBGAs/s1600/update-status-keren.gif';
+ad[3]='https://2.bp.blogspot.com/-Q5hU9T_CbXw/W0NJvXKU63I/AAAAAAAAMDc/BHoaEdjOn6wDuzQosRwpSoxDXyvIzJu7QCLcBGAs/s1600/statuswa.gif';
+
+
+
+var links=new Array()
+links[0]='https://play.google.com/store/apps/details?id=com.nana.autoteksterbaru';
+links[1]='https://play.google.com/store/apps/details?id=com.nana.buatdpkeren';
+links[2]='https://goo.gl/hmhd54';
+links[3]='https://play.google.com/store/apps/details?id=com.nana.katastatus';
+
+
+
+
+
+var xy=Math.floor(Math.random()*ad.length);
+document.write('<a href="'+links[xy]+'" rel="nofollow" target="_blank"><img src="'+ad[xy]+'" width="'+img_width+'" height="'+img_height+'"></a>');
+
+//]]>
